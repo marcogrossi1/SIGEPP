3fd51fccbe06d48e625b3efc905dc2b498841dd7
CaioSilveiraFigueiredo <caiosilveirafig@gmail.com>
2024-12-21 21:26:14 -0300

    🧪 Atualização de testes

--- PIPA/src/main/java/proj/PipaApplication.java
+++ PIPA/src/main/java/proj/PipaApplication.java
@@ -3,6 +3,7 @@ package proj;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//
 @SpringBootApplication
 public class PipaApplication {
 
