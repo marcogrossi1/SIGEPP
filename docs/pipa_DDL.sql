de3069d06b3c3204945d3e6a82122d6dd1f5ebfd
CaioSilveiraFigueiredo <127897116+CaioSilveiraFigueiredo@users.noreply.github.com>
2025-01-07 09:51:38 -0300

    🔖 Update pipa_DDL.sql

--- docs/pipa_DDL.sql
+++ docs/pipa_DDL.sql
@@ -1,3 +1,15 @@
+-- -----------------------------------------------------
+-- Table Usuario
+-- -----------------------------------------------------
+CREATE TABLE Usuario (
+  id BIGINT NOT NULL AUTO_INCREMENT,
+  nome VARCHAR(45) NOT NULL,
+  senha VARCHAR(255) NOT NULL,
+  role VARCHAR(45) NOT NULL,
+  PRIMARY KEY (id),
+  UNIQUE INDEX nome_UNIQUE (nome ASC) )
+ENGINE = InnoDB default character set = utf8;
+
 
 -- -----------------------------------------------------
 -- Table Aluno
@@ -9,34 +21,19 @@ CREATE TABLE Aluno (
   curso VARCHAR(255) NOT NULL,
   campus VARCHAR(255) NOT NULL,
   email VARCHAR(255) NOT NULL,
-  senha VARCHAR(255) NOT NULL,
   periodo VARCHAR(255) NOT NULL,
-  
+  Usuario_id BIGINT NULL,
   PRIMARY KEY (id),
   UNIQUE INDEX cpf_UNIQUE (cpf ASC) ,
   INDEX nome_INDEX (nome ASC) ,
-  UNIQUE INDEX email_UNIQUE (email ASC) )
+  UNIQUE INDEX email_UNIQUE (email ASC) ,
+  INDEX fk_Aluno_Usuario1_idx (Usuario_id ASC) ,
+  CONSTRAINT fk_Aluno_Usuario1
+    FOREIGN KEY (Usuario_id)
+    REFERENCES Usuario (id)
+    )
 ENGINE = InnoDB default character set = utf8;
 
--- -----------------------------------------------------
--- Table Empresa
--- -----------------------------------------------------
-CREATE TABLE Empresa (
-  id BIGINT NOT NULL AUTO_INCREMENT,
-  cnpj VARCHAR(20) NOT NULL,
-  nome VARCHAR(255) NOT NULL,
-  endereco VARCHAR(255) NOT NULL,
-  website VARCHAR(255) NOT NULL,
-  area VARCHAR(255) NOT NULL,
-  telefone VARCHAR(20) NOT NULL,
-  email VARCHAR(255) NOT NULL,
-  senha VARCHAR(255) NOT NULL,
-  
-  PRIMARY KEY (id),
-  UNIQUE INDEX cnpj_UNIQUE (cnpj ASC),
-  UNIQUE INDEX email_UNIQUE (email ASC),
-  INDEX nome_INDEX (nome ASC) )
-ENGINE = InnoDB default character set = utf8;
 
 -- -----------------------------------------------------
 -- Table Estagio
@@ -44,15 +41,13 @@ ENGINE = InnoDB default character set = utf8;
 CREATE TABLE Estagio (
   id BIGINT NOT NULL AUTO_INCREMENT,
   empresa VARCHAR(255) NOT NULL,
-  descricao VARCHAR(500) NOT NULL,
+  descricao VARCHAR(500) NULL,
   carga_horaria INT NULL,
   vagas INT NULL,
-  requisito VARCHAR(255) NOT NULL,
-  salario VARCHAR(255) NOT NULL,
-  
+  requisito VARCHAR(255) NULL,
   PRIMARY KEY (id),
   INDEX empresa_INDEX (empresa ASC) )
-ENGINE = InnoDB;
+ENGINE = InnoDB default character set = utf8;
 
 
 -- -----------------------------------------------------
@@ -66,10 +61,9 @@ CREATE TABLE Projeto (
   carga_horaria INT NULL,
   vagas INT NULL,
   requisito VARCHAR(255) NULL,
-  
   PRIMARY KEY (id),
   UNIQUE INDEX nome_UNIQUE (nome ASC) )
-ENGINE = InnoDB;
+ENGINE = InnoDB default character set = utf8;
 
 
 -- -----------------------------------------------------
@@ -78,17 +72,18 @@ ENGINE = InnoDB;
 CREATE TABLE Aluno_has_Projeto (
   aluno_id BIGINT NOT NULL,
   projeto_id BIGINT NOT NULL,
-  
-  PRIMARY KEY (Aluno_id, Projeto_id),
-  INDEX fk_Aluno_has_Projeto_Projeto1_idx (Projeto_id ASC) ,
-  INDEX fk_Aluno_has_Projeto_Aluno_idx (Aluno_id ASC) ,
+  PRIMARY KEY (aluno_id, projeto_id),
+  INDEX fk_Aluno_has_Projeto_Projeto1_idx (projeto_id ASC) ,
+  INDEX fk_Aluno_has_Projeto_Aluno_idx (aluno_id ASC) ,
   CONSTRAINT fk_Aluno_has_Projeto_Aluno
-    FOREIGN KEY (Aluno_id)
-    REFERENCES Aluno (id),
+    FOREIGN KEY (aluno_id)
+    REFERENCES Aluno (id)
+    ,
   CONSTRAINT fk_Aluno_has_Projeto_Projeto1
-    FOREIGN KEY (Projeto_id)
-    REFERENCES Projeto (id))
-ENGINE = InnoDB;
+    FOREIGN KEY (projeto_id)
+    REFERENCES Projeto (id)
+    )
+ENGINE = InnoDB default character set = utf8;
 
 
 -- -----------------------------------------------------
@@ -97,17 +92,59 @@ ENGINE = InnoDB;
 CREATE TABLE Aluno_has_Estagio (
   aluno_id BIGINT NOT NULL,
   estagio_id BIGINT NOT NULL,
-  
-  PRIMARY KEY (Aluno_id, Estagio_id),
-  INDEX fk_Aluno_has_Estagio_Estagio1_idx (Estagio_id ASC) ,
-  INDEX fk_Aluno_has_Estagio_Aluno1_idx (Aluno_id ASC) ,
+  PRIMARY KEY (aluno_id, estagio_id),
+  INDEX fk_Aluno_has_Estagio_Estagio1_idx (estagio_id ASC) ,
+  INDEX fk_Aluno_has_Estagio_Aluno1_idx (aluno_id ASC) ,
   CONSTRAINT fk_Aluno_has_Estagio_Aluno1
-    FOREIGN KEY (Aluno_id)
-    REFERENCES  Aluno (id),
+    FOREIGN KEY (aluno_id)
+    REFERENCES Aluno (id)
+    ,
   CONSTRAINT fk_Aluno_has_Estagio_Estagio1
-    FOREIGN KEY (Estagio_id)
-    REFERENCES Estagio (id))
-ENGINE = InnoDB;
+    FOREIGN KEY (estagio_id)
+    REFERENCES Estagio (id)
+    )
+ENGINE = InnoDB default character set = utf8;
+
+-- -----------------------------------------------------
+-- Table Professor
+-- -----------------------------------------------------
+CREATE TABLE Professor (
+  id BIGINT NOT NULL AUTO_INCREMENT,
+  nome VARCHAR(255) NOT NULL,
+  Usuario_id BIGINT NULL,
+  PRIMARY KEY (id),
+  INDEX fk_Professor_Usuario1_idx (Usuario_id ASC) ,
+  CONSTRAINT fk_Professor_Usuario1
+    FOREIGN KEY (Usuario_id)
+    REFERENCES Usuario (id)
+    )
+ENGINE = InnoDB default character set = utf8;
+
+
+-- -----------------------------------------------------
+-- Table Empresa
+-- -----------------------------------------------------
+CREATE TABLE Empresa (
+  id BIGINT NOT NULL  AUTO_INCREMENT,
+  cnpj VARCHAR(20) NOT NULL,
+  nome VARCHAR(255) NOT NULL,
+  endereco VARCHAR(255) NOT NULL,
+  website VARCHAR(255) NOT NULL,
+  area VARCHAR(255) NOT NULL,
+  telefone VARCHAR(20) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  Usuario_id BIGINT NULL,
+  PRIMARY KEY (id),
+  INDEX fk_Empresa_Usuario1_idx (Usuario_id ASC),
+  UNIQUE INDEX cnpj_UNIQUE (cnpj ASC),
+  UNIQUE INDEX email_UNIQUE (email ASC),
+  INDEX nome_INDEX (nome ASC),
+  CONSTRAINT fk_Empresa_Usuario1
+    FOREIGN KEY (Usuario_id)
+    REFERENCES Usuario (id)	
+    )
+ENGINE = InnoDB default character set = utf8;
+
 
 -- -----------------------------------------------------
 -- Table Empresa_has_Estagio
