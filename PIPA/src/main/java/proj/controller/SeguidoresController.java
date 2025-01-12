6f42240e6e00acb1827b91f93037c4b7c068c063
arthur-kerlan <arthur.mf.kerlan@gmail.com>
2025-01-12 16:54:41 -0300

    :sparkles: feat: CDU-04: Adicionando lista de pessoas seguidas + método toggle

--- PIPA/src/main/java/proj/controller/SeguidoresController.java
+++ PIPA/src/main/java/proj/controller/SeguidoresController.java
@@ -1,5 +1,7 @@
 package proj.controller;
 
+import java.security.Principal;
+import java.sql.Connection;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -13,6 +15,7 @@ import proj.model.Usuario;
 
 import proj.dao.HDataSource;
 import proj.dao.SeguidoresDao;
+import proj.dao.UsuarioDao;
 
 
 @Controller
@@ -23,26 +26,79 @@ public class SeguidoresController {
     HDataSource ds;
 
 	@GetMapping
-	public String mostraSeguidores(Model model, @RequestParam("id") long id) {
+	public String mostraSeguidores(Model model, Principal principal) {
             
             Seguidores s;
             
-            try{
-                
-                s = SeguidoresDao.listSeguidores( ds.getConnection(), id);
-                
-                for(Usuario u : s.getSeguidores()){
-                    
-                    System.out.println(u.getNome());
-                }
-                
-            
-            }
-                catch(Exception e) {
-                    e.printStackTrace();
-                    return "erro";
-            }
-                    return "verSeguidores";
-            }
-}
+            try (Connection conn = ds.getConnection()) {
 
+                //retorna o usuário que faz a requisição
+                Usuario  usuarioLogged = UsuarioDao.getByNome( conn, principal.getName() );
+                
+                s = SeguidoresDao.listSeguidores( conn, usuarioLogged.getId() );
+                    
+                model.addAttribute("Seguidores", s);
+
+            }
+            catch(Exception e) {
+                e.printStackTrace();
+                return "erro";
+            }
+
+                return "verSeguidores";
+        }
+
+        @GetMapping("/seguidos")
+        public String MostrarSeguindo(Model model, Principal principal) {
+        
+        Seguidores s;
+        
+        try (Connection conn = ds.getConnection()) {
+            //retorna o usuário que faz a requisição
+            Usuario  usuarioLogged = UsuarioDao.getByNome( conn, principal.getName() );
+            
+            s = SeguidoresDao.listSeguidos( conn, usuarioLogged.getId() );
+                
+            model.addAttribute("Seguidores", s);
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+            return "erro";
+        }
+            return "verSeguidores";
+    }
+
+    @GetMapping("/toggle")
+    public String toogleSeguidor(Model model, @RequestParam("id") long seguindo_id, Principal principal){
+        /*  Se estiver seguindo
+         * 
+         */
+        try (Connection conn = ds.getConnection()) {
+
+            //retorna o usuário que faz a requisição
+            Usuario  usuarioLogged = UsuarioDao.getByNome( conn, principal.getName());
+
+            //testa se o usuário que será seguido existe
+            Usuario uSeguido = UsuarioDao.get(conn, seguindo_id);//retorna uma excessão se não existir
+
+            long seguidor_id = usuarioLogged.getId();
+
+            if(SeguidoresDao.isFollowing(conn, seguidor_id, seguindo_id)){
+
+                SeguidoresDao.deleteFollowRelation(conn, seguidor_id, seguindo_id);
+                model.addAttribute("message", "Deixou de seguir "+uSeguido.getNome());
+            }
+
+            else{
+                SeguidoresDao.insertFollowRelation( conn, seguidor_id, seguindo_id );
+                model.addAttribute("message", "Seguiu "+uSeguido.getNome());
+            }
+
+            return "followingMessage";
+        }
+        catch(Exception e){
+            model.addAttribute("message", e.getMessage());
+            return "erro";
+        }
+    }
+}
