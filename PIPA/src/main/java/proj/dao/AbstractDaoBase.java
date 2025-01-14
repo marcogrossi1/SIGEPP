e88295f5f344c508413d2cef82809e19096553b0
Asafe Rodrigues Garcia <asafeariel1@gmail.com>
2025-01-14 00:57:50 -0300

    Reapply "Merge remote-tracking branch 'origin/dev' into Asafe"
    This reverts commit 016f34bfebc0d58172382b64145c9ea684619406.

--- PIPA/src/main/java/proj/dao/AbstractDaoBase.java
+++ PIPA/src/main/java/proj/dao/AbstractDaoBase.java
@@ -1,47 +1,54 @@
 package proj.dao;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.sql.Statement;
 
-public abstract class AbstractDaoBase {
-
-    public static Connection getConnection() throws SQLException {
-        String url = "jdbc:mysql://localhost:3306/pipa?createDatabaseIfNotExist=true&characterEncoding=UTF-8"; // Altere para a URL do seu banco de dados
-        String user = "root"; // Altere para seu usuário
-        String password = "1234"; // Altere para sua senha
-        return DriverManager.getConnection(url, user, password);
-    }
-
-    protected static void rollbackConnection(Connection conn) {
-        try {
+public abstract class AbstractDaoBase
+{
+    protected static void rollbackConnection(Connection conn)
+    {
+        try
+        {
             if (conn != null) conn.rollback();
-        } catch (Exception e) {
+        }
+        catch (Exception e)
+        {
             conn = null;
         }
     }
-
-    protected static void closeResource(Statement ps, ResultSet rs) {
-        try {
+    
+    protected static void closeResource(Statement ps, ResultSet rs)
+    {
+        try
+        {
             if (rs != null) rs.close();
-        } catch (Exception e) {
+        }
+        catch (Exception e)
+        {
             rs = null;
         }
 
-        try {
+        try
+        {
             if (ps != null) ps.close();
-        } catch (Exception e) {
+        }
+        catch (Exception e)
+        {
             ps = null;
         }
     }
 
-    protected static void closeResource(Statement ps) {
-        try {
+    protected static void closeResource(Statement ps)
+    {
+        try
+        {
             if (ps != null) ps.close();
-        } catch (Exception e) {
+        }
+        catch (Exception e)
+        {
             ps = null;
         }
     }
+
 }
