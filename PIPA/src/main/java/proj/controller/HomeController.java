0a37dcadb1b3c8bbbdca84c3796c19ee686b2e6b
Gabriel Henrique da Costa Tavares <henriquepokacha@gmail.com>
2025-02-01 19:03:59 -0300

    Alterações

--- PIPA/src/main/java/proj/controller/HomeController.java
+++ PIPA/src/main/java/proj/controller/HomeController.java
@@ -1,14 +1,9 @@
 package proj.controller;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.InputStream;
 import java.security.Principal;
 import java.sql.Connection;
-import java.util.ArrayList;
-import java.util.Base64;
-import java.util.List;
 
+import org.apache.commons.codec.digest.DigestUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -16,38 +11,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.multipart.MultipartFile;
 
-import com.itextpdf.text.BaseColor;
-import com.itextpdf.text.Document;
-import com.itextpdf.text.Font;
-import com.itextpdf.text.FontFactory;
-import com.itextpdf.text.Image;
-import com.itextpdf.text.Paragraph;
-import com.itextpdf.text.pdf.PdfWriter;
-
-import org.springframework.web.bind.annotation.ModelAttribute;
-
-import proj.dao.HDataSource;
-import proj.dao.AdministradorDao;
 import proj.dao.AlunoDao;
-import proj.dao.ProfessorDao;
-import proj.dao.ProjetoDao;
-import proj.dao.SecaoDao;
-import proj.dao.SeguidoresDao;
-import proj.dao.TopicoDao;
+import proj.dao.HDataSource;
 import proj.dao.UsuarioDao;
-import proj.dao.EmpresaDao;
-import proj.dao.EstagioDao;
-import proj.model.Administrador;
 import proj.model.Aluno;
-import proj.model.Professor;
-import proj.model.Projeto;
-import proj.model.Secao;
-import proj.model.Topico;
 import proj.model.Usuario;
-import proj.model.Empresa;
-import proj.model.Estagio;
 
 @Controller
 @RequestMapping("/")
@@ -62,72 +31,101 @@ public class HomeController {
 	}
 	
 	@GetMapping("/login")
-	public String mostrarLogin(Model model, Principal principal) 
-	throws Exception {
-		try(Connection conn = ds.getConnection()) {
-			Usuario u = UsuarioDao.getByNome(conn, principal.getName());
-			
-	    	model.addAttribute("usuarioId", u.getId());
-	    	return "login";
-		}
-		
-		catch(Exception e) {
-			return "erro";
-		}
+	public String login() {
+		return "login";
 	}
 	
 	@GetMapping("/cadastroAluno")
-	public String mostrarCadastroAluno(@RequestParam("id") Long Usuario_id, Model model, Principal principal) 
-	throws Exception {
-		try(Connection conn = ds.getConnection()) {
-			Usuario u = UsuarioDao.getByNome(conn, principal.getName());
-        	model.addAttribute("usuario", u);
-        	
-        	if (u.getRole().equals("Aluno")) {
-        	    Aluno a = AlunoDao.get(conn, Usuario_id);
-        	    model.addAttribute("aluno", a);
-        	    
-        	    String nome = a.getNome();
-        	    String cpf = a.getCpf();
-        	    String curso = a.getCurso();
-        	    String campus = a.getCampus();
-        	    String email = a.getEmail();
-        	    String periodo = a.getPeriodo();
-        	    String telefone = a.getTelefone();
-        	    String senha = u.getSenha();
+	public String mostraPaginaDeCadastro() {
+		return "cadastroAluno";
+	}
+	
+	@PostMapping("/criarAluno")
+	public String criaNovoAluno(
+		@RequestParam("id") Long UsuarioId, 
+	    @RequestParam(value = "nome", required = false) String nome,
+	    @RequestParam(value = "cpf", required = false) String cpf,
+	    @RequestParam(value = "curso", required = false) String curso,
+	    @RequestParam(value = "campus", required = false) String campus,
+	    @RequestParam(value = "email", required = false) String email,
+	    @RequestParam(value = "periodo", required = false) String periodo,
+	    @RequestParam(value = "telefone", required = false) String telefone,
+	    @RequestParam(value = "senha", required = false) String senha,
+	    Principal principal, Model model) throws Exception {
+	    try (Connection conn = ds.getConnection()) {
+	        UsuarioDao.insert(conn, u);
+	    	AlunoDao.insert(conn, a);
 
-        	    model.addAttribute("nome", nome);
-        	    model.addAttribute("cpf", cpf);
-        	    model.addAttribute("curso", curso);
-        	    model.addAttribute("campus", campus);
-        	    model.addAttribute("email", email);
-        	    model.addAttribute("periodo", periodo);
-        	    model.addAttribute("telefone", telefone);
-        	    model.addAttribute("senha", senha);
-
-        	    return "cadastroAluno";
-        	}
-        	
-        	/*else if (u.getRole().equals("Professor")) {
-        		Professor p = ProfessorDao.get(conn, Usuario_id);
-        		return "/configPerfil/contaConfigPerfil";
-        	}
-        	else if (u.getRole().equals("Administrador")) {
-        		Administrador admin = AdministradorDao.get(conn, Usuario_id);
-        		return "/configPerfil/contaConfigPerfil";
-        	}*/
-			else {
-				return mostraPaginaDeErro(model, "Você não tem permissão para acessar esta página.");
-			}
-		}
-		
-		catch(Exception e) {
-			return "erro";
-		}
+	        if (senha != null && !senha.isEmpty()) {
+		        String senhaCriptografada = DigestUtils.sha512Hex(senha);
+		        u.setSenha(senhaCriptografada);
+		        UsuarioDao.updateForSenha(conn, UsuarioId, senhaCriptografada);
+	        }
+	        
+	        if (u.getRole().equals("Aluno")) {
+	            Aluno a = AlunoDao.get(conn, UsuarioId);
+	            
+	            if (nome != null && !nome.isEmpty()) {
+	                a.setNome(nome);
+	            }
+	            
+	            if (curso != null && !curso.isEmpty()) {
+	                a.setCurso(curso);
+	            }
+	            if (campus != null && !campus.isEmpty()) {
+	                a.setCampus(campus);
+	            }
+	            if (email != null && !email.isEmpty()) {
+	                a.setEmail(email);
+	            }
+	            if (periodo != null && !periodo.isEmpty()) {
+	                a.setPeriodo(periodo);
+	            }
+	            if (telefone != null && !telefone.isEmpty()) {
+	                a.setTelefone(telefone);
+	            }
+	            
+	            if (cpf != null && !cpf.isEmpty()) {
+	                a.setCpf(cpf);
+	                u.setNome(cpf);
+	            }
+	            
+	            AlunoDao.update(conn, a);
+                UsuarioDao.update(conn, u);
+                conn.commit();
+	            
+	            System.out.println("Valores recebidos:");
+	            System.out.println("Nome: " + nome);
+	            System.out.println("CPF: " + cpf);
+	            System.out.println("Curso: " + curso);
+	            System.out.println("Campus: " + campus);
+	            System.out.println("Email: " + email);
+	            System.out.println("Período: " + periodo);
+	            System.out.println("Telefone: " + telefone);
+	            System.out.println("Senha: " + senha);
+	            
+	        }
+		return "redirect:/aluno";
 	}
 	
 	public String mostraPaginaDeErro(Model model, String message) {
 		model.addAttribute("message",message);
 		return "erro";
-	}
+	}	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
