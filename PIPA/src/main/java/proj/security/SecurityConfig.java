6d6406faadb9d496afcecef5e83e75fd8eb90512
RAFAELLA FERNANDES CORTEZ DO AMARAL <127240010+RafaellaAmaral@users.noreply.github.com>
2025-02-03 21:52:33 -0300

    🔧 Adiciona commit para transações no banco de dados e ajusta permissões de acesso nas configurações de segurança

--- PIPA/src/main/java/proj/security/SecurityConfig.java
+++ PIPA/src/main/java/proj/security/SecurityConfig.java
@@ -25,7 +25,7 @@ public class SecurityConfig {
                     .requestMatchers("/empresa/**").hasRole("Empresa")
                     .requestMatchers("/administrador/**").hasRole("Administrador")
                     .requestMatchers("/pesquisa/**").hasAnyRole("Professor","Empresa")
-                    .requestMatchers("/cadastro", "/cadastroAluno", "/criarAluno", "/cadastroProfessor", "/criarProfessor", "/cadastroEmpresa", "/criarEmpresa", "/cadastroAdministrador", "criarAdministrador").permitAll()
+                    .requestMatchers("/cadastro", "/cadastroAluno", "/criarAluno", "/cadastroProfessor", "/criarProfessor", "/cadastroEmpresa", "/criarEmpresa").permitAll()
                     .anyRequest().authenticated()
             )
             .csrf(csrf -> csrf.disable()) 
